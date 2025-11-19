2e987dfd6a62e48111623407e6a2292fe0f70381
veerendra <singampalliveerendra5@gmail.com>
2025-11-19 14:58:36 +0530

    fixed coding style

--- product.py
+++ product.py
@@ -3,5 +3,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 num3 = int(input("Enter the three number: "))
 product = num1 * num2 * num3
-print("The product of ", num1,", ", num2, " and ", num3, " is ", product, sep ="", end = ".\n")
+print("The product of ", num1, ", ", num2, " and ", num3, " is ", product, sep="", end=".\n")
 
