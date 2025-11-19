492306ae821409d582ae74975df0f71439ec6f97
veerendra <singampalliveerendra5@gmail.com>
2025-11-19 14:53:44 +0530

    changed

---
+++ mul.py
@@ -0,0 +1,7 @@
+print("To multiply three numbers. ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the three number: "))
+product = num1 * num2 * num3
+print("The product of ", num1,", ", num2, " and ", num3, " is ", product, sep ="", end = ".\n")
+
