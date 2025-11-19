83bf1099d87ca09140bb9b67170fbee7410a2d8f
veerendra <singampalliveerendra5@gmail.com>
2025-11-19 14:12:50 +0530

    changes added

--- sum.py
+++ sum.py
@@ -1,6 +1,6 @@
 print("To add two numbers: ")
-num1 = int(input("Enter the first number: ")
-num2 = int(input("Enter the second number: ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 sum = num1 + num2
-print("The sum of " num1, " and ", num2 " is ", sum, end = ".\n")
+print("The sum of ", num1, " and ", num2, " is ", sum, end = ".\n")
 
