3152c374005c07c5c1888f9a84a31db2ac28ba59
veerendra <singampalliveerendra5@gmail.com>
2025-11-21 15:37:49 +0530

    updated

--- natural_numbers_up_to_n.py
+++ natural_numbers_up_to_n.py
@@ -1,7 +1,7 @@
-count = int(input("Upto which number you want to print natural numbers? "))
+count = int(input("Up to which number you want to print natural numbers? "))
 counter = 1
 print("The first", count, "natural numbers are ", end="")
 while (counter < count):
     print(counter, end=", ")
-    counter = counter +1
+    counter = counter + 1
 print(counter, end=".\n")
