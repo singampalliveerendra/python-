e1193493b273a460d30fb097e6ae7654cb47a01d
veerendra <singampalliveerendra5@gmail.com>
2025-11-22 12:09:19 +0530

    updated

--- even_numbers_up_to_n.py
+++ even_numbers_up_to_n.py
@@ -1,7 +1,7 @@
-number=(int(input("Up to which number you want to print even numbers? ")))
-counter=0
-print("The even numbers up to", number, "are ", end ="" )
-while (counter < number -1):
-    print (counter, end=", ")
+number = (int(input("Up to which number you want to print even numbers? ")))
+counter = 0
+print("The even numbers up to", number, "are ", end="")
+while (counter < number - 1):
+    print(counter, end=", ")
     counter = counter + 2
-print (counter, ".", sep="")
+print(counter, ".", sep="")
