22a3bdb8d53468be8681b8c37e599b98a458fa9f
veerendra <singampalliveerendra5@gmail.com>
2025-11-22 14:45:39 +0530

    new version

---
+++ product_version2.py
@@ -0,0 +1,7 @@
+print("To multiply three numbers.")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+product = num1*num2*num3
+print(f"The product of {num1}, {num2} and {num3} is {product}.")
+
