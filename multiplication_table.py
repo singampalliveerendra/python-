d5c7b137aefed58095ecd450a1be426c507dba64
veerendra <singampalliveerendra5@gmail.com>
2025-11-26 11:18:37 +0530

    updated

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,7 +1,7 @@
-number=int(input("Which multiplication table do you want to print? "))
-count=10
-counter=0
-print(f("The multiplication table of {number} is "))
-while (counter <= count):
+number = int(input("Which multiplication table you want to print? "))
+count = 10
+counter = 0
+print(f"The multiplication table of {number} is:")
+while (counter < count):
     counter = counter + 1
-    print (f("{number} x {counter} = {number * counter}"))
+    print(f"{number:2} x {counter:2} = {number * counter:3}")
