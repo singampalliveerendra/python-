5248b808ce09546c9bc5ac8db744dd6ae612f47e
veerendra <singampalliveerendra5@gmail.com>
2025-11-27 11:28:12 +0530

    updated

--- n_power_n_series_up_to_n.py
+++ n_power_n_series_up_to_n.py
@@ -1,11 +1,15 @@
 count = int(input("Up to which number you want to print the N power N series? "))
-counter = 1
+counter = 2
 result = 1
-power = 1
-print(f"The N power N series up to {count} is {counter}", end="")
+print(f"The N power N series up to {count} is 1", end="")
 while (result <= count):
+    result = 1
+    counter1 = 1
+    while (counter1 <= counter):
+        result = result * counter
+        counter1 = counter1 + 1
+    if (result <= count):
+        print(f", {result}", end="")
     counter = counter + 1
-    result = result * counter
-    power = power + 1
-    print(", ", result, end="")
 print(".")
+
